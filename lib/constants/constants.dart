d447b1734e8da495adee24fe93be5467d6da823c
Mohamed Ben Massouda <benmassoudamohamed1@gmail.com>
2024-03-20 14:52:16 +0100

    Saving the repeated values in a constant variables

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final backgroundColor = Colors.grey[300];
+final primaryColor = Colors.grey[800];
+final accentColor = Colors.grey[800];
+final iconColor = Colors.grey[800];
